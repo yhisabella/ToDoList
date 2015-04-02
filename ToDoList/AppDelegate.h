d864736af079fbb3d396715c26875591cb4624fd
Sundy <Sundy@dhcp-10-5-10-226.wireless.rochester.edu>
2015-04-02 16:47:27 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Sundy on 4/2/15.
+//  Copyright (c) 2015 Yuhui Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Sundy on 4/2/15.
+//  Copyright (c) 2015 Yuhui Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
